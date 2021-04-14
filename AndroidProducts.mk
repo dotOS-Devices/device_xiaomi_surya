24452cfa4a0b4b6fae08531a78b0be86de7182c2
palaych-star <thepsambit4@gmail.com>
2021-04-14 01:43:32 +0000

    dot: Dot-OS bringup for surya

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_surya.mk
+    $(LOCAL_DIR)/dot_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_surya-user \
-    fluid_surya-userdebug \
-    fluid_surya-eng
+    dot_surya-user \
+    dot_surya-userdebug \
+    dot_surya-eng
--- dot_surya.mk
+++ dot_surya.mk
@@ -6,11 +6,13 @@
 
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Fluid-ROM stuff.
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+
+DOT_OFFICIAL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_surya
+PRODUCT_NAME := dot_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
@@ -19,6 +21,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_BOOT_ANIMATION_RES := 1080
-PRODUCT_PRODUCT_PROPERTIES += \
-           ro.fluid.maintainer=Sambit \
-           ro.fluid.cpu=SDM732G
+TARGET_FACE_UNLOCK_SUPPORTED := true
