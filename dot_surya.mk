8e3aa7ceb286bd035f0a81891cf0200bbf13d782
palaych-star <thepsambit4@gmail.com>
2021-04-14 01:43:32 +0000

    dot: Dot-OS bringup for surya

--- dot_surya.mk
+++ dot_surya.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Fluid-ROM stuff.
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_surya
+PRODUCT_NAME := dot_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
@@ -19,6 +19,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_BOOT_ANIMATION_RES := 1080
-PRODUCT_PRODUCT_PROPERTIES += \
-           ro.fluid.maintainer=Sambit \
-           ro.fluid.cpu=SDM732G
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,9 +16,9 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-FLUID_ROOT="${MY_DIR}/../../.."
+DOT_ROOT="${MY_DIR}/../../.."
 
-HELPER="${FLUID_ROOT}/vendor/fluid/build/tools/extract_utils.sh"
+HELPER="${DOT_ROOT}/vendor/dot/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -26,7 +26,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${FLUID_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${DOT_ROOT}"
 
 # Copyright headers and guards
 write_headers
