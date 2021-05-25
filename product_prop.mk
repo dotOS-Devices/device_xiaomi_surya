81de16bee4b65be1d9f40628eb2b630afe896508
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-05-25 15:13:51 -0300

    surya: set blur by default
    * surya can handle it without problems
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -36,7 +36,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1 \
-    persist.sys.sf.disable_blurs=1
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
+
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
